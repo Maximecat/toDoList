6ff6b5ef53b39b7f30d88159a440315f8c207612
Maxime CAT <maximecat@outlook.fr>
2023-10-24 14:35:21 +0200

    App start add remove

--- public/js/controllers/App.js
+++ public/js/controllers/App.js
@@ -1,28 +1,35 @@
-import { ToDoFactory } from "../factories/ToDoFactory.js";
 import { ToDoService } from "../service/ToDoService.js";
 
 class App {
     toDoService;
     toDoContainer;
-    toDo;
+    todos;
 
     constructor() {
         this.toDoService = new ToDoService();
         this.toDoContainer = document.querySelector('#main')
-        this.toDo = [];
+        this.todos = [];
         this.getToDoDatas();
     }
 
     async getToDoDatas() {
-        this.toDo = await this.toDoService.getToDoList();
+        this.todos = await this.toDoService.getToDoList();
         this.displayToDo();
     }
 
     displayToDo() {
-        for (const toDo of this.toDo) {
-            const toDoFactory = new ToDoFactory(toDo)
-            const toDoCard = toDoFactory.createToDoCard();
+        for (const toDo of this.todos) {
+            const toDoCard = toDo.createToDoCard();
+            console.log(toDoCard);
+            console.log(toDoCard.querySelector(".todo-remove"));
 
+            const todoRemoveButton = toDoCard.querySelector(".todo-remove");
+            todoRemoveButton.addEventListener("click", (e) => {
+                console.log("Remove todo : " + toDo.id)
+            });
+
+            // Je veut que quand tu clique sur le boutton de remove,
+            // ca affiche dans la console "remove" et l'id de la carte sur laquelle j'ai cliqué
             this.toDoContainer.appendChild(toDoCard)
         }
     }
