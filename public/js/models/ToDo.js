1d0e308f3f83e8f9e13ca5e93ce91589d8c10b94
Maxime CAT <maximecat@outlook.fr>
2023-10-24 14:34:48 +0200

    Model card todo

--- public/js/models/ToDo.js
+++ public/js/models/ToDo.js
@@ -4,10 +4,35 @@ export class ToDo {
     completed;
     userId;
 
-    constructor(ToDoElement) {
-        this.id = ToDoElement.id;
-        this.todo = ToDoElement.todo;
-        this.completed = ToDoElement.completed;
-        this.userId = ToDoElement.userId;
+    constructor(toDoElement) {
+        this.id = toDoElement.id;
+        this.todo = toDoElement.todo;
+        this.completed = toDoElement.completed;
+        this.userId = toDoElement.userId;
+    }
+
+    createToDoCard() {
+        const articletoDo = document.createElement('article')
+        articletoDo.classList.add('todo-card')
+        articletoDo.id = this.id
+
+        const inputCheckBox = document.createElement('input')
+        inputCheckBox.classList.add('todo-checkbox')
+        inputCheckBox.type = 'checkbox'
+        inputCheckBox.checked = this.completed
+
+        const paragraphToDo = document.createElement('p')
+        paragraphToDo.classList.add('todo-paragraph')
+        paragraphToDo.innerText = this.todo
+
+        const toDoRemove = document.createElement('button')
+        toDoRemove.classList.add('todo-remove')
+        toDoRemove.innerText = 'Remove'
+
+        articletoDo.appendChild(inputCheckBox)
+        articletoDo.appendChild(paragraphToDo)
+        articletoDo.appendChild(toDoRemove)
+
+        return articletoDo;
     }
 }
