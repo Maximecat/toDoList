640d31f9a9d5342cc03c9db064aa900c2cfa865f
Maxime CAT <maximecat@outlook.fr>
2023-10-31 14:41:11 +0100

    App

--- public/js/controllers/App.js
+++ public/js/controllers/App.js
@@ -4,12 +4,15 @@ class App {
     toDoService;
     toDoContainer;
     todos;
+    todoForm;
 
     constructor() {
         this.toDoService = new ToDoService();
         this.toDoContainer = document.querySelector('#main')
         this.todos = [];
+        this.todoForm = document.querySelector('#form-todo')
         this.getToDoDatas();
+        this.addTodo();
     }
 
     async getToDoDatas() {
@@ -18,6 +21,8 @@ class App {
     }
 
     displayToDo() {
+        // this.toDoContainer.innerHTML = ""
+
         for (const toDo of this.todos) {
             const toDoCard = toDo.createToDoCard();
 
@@ -25,25 +30,26 @@ class App {
             const todoCheckbox = toDoCard.querySelector('.todo-checkbox');
             const todoText = toDoCard.querySelector('.todo-paragraph')
 
-            const clickForChecked = todoCheckbox.addEventListener("click", () => {
-                if (todoCheckbox.checked === false) {
-                    todoText.style.cssText = "text-decoration: none;"
-                } else {
-                    todoText.style.cssText = "text-decoration: line-through;"
-                }
+            todoCheckbox.addEventListener("click", () => {
+                this.toDoService.updateTodoElement(toDoCard.id, todoCheckbox.checked)
+                    .then(updatedTodo => {
+                        if (updatedTodo.completed === false) {
+                            todoText.style.cssText = "text-decoration: none;"
+                        } else {
+                            todoText.style.cssText = "text-decoration: line-through;"
+                        }
+                    })
             })
-
-            if (todoCheckbox.checked === true) {
-                todoText.style.cssText = "text-decoration: line-through;"
-            } else {
-                clickForChecked;
-            }
 
             todoRemoveButton.addEventListener("click", () => {
                 if (todoCheckbox.checked === false) {
-                    console.log("You must validate the checkbox to remove the todo");
+                    alert("You must validate the checkbox to remove the todo");
                 } else {
-                    toDoCard.remove()
+                    this.toDoService.deleteTodoElement(toDoCard.id)
+                        .then(deletedTodo => {
+                            toDoCard.remove()
+                            console.log(deletedTodo);
+                        })
                     console.log("Remove todo : " + toDoCard.id)
                 }
             });
@@ -52,40 +58,13 @@ class App {
         }
     }
 
-    // addTodo() {
-    //     const addInput = document.querySelector('.input-add-todo')
-    //     console.log(addInput);
-    //     const toDoContainer = document.querySelector('#main')
-    //     console.log(toDoContainer);
-    //     const addButton = document.querySelector('.button-add-todo')
-    //     console.log(addButton);
+    addTodo() {
+        this.todoForm.addEventListener("submit", (e) => {
+            e.preventDefault();
 
-    //     addButton.addEventListener("click", () => {
-    //         if (addInput.value === '') {
-    //             console.log("Nothing is written");
-    //         } else {
-    //             const article = document.createElement("article");
-    //             article.classList.add('todo-card')
-
-    //             const inputcheck = document.createElement('input')
-    //             inputcheck.classList.add('todo-checkbox')
-    //             inputcheck.type = 'checkbox'
-
-    //             const paragraph = document.createElement('p')
-    //             paragraph.classList.add('todo-paragraph')
-    //             paragraph.innerHTML = addInput.value;
-
-    //             const remove = document.createElement('button')
-    //             remove.classList.add('todo-remove')
-    //             remove.innerText = 'Remove'
-
-    //             toDoContainer.appendChild(article);
-    //             article.appendChild(inputcheck)
-    //             article.appendChild(paragraph);
-    //             article.appendChild(remove)
-    //         }
-    //     });
-    // }
+            console.log(e.srcElement[0].value);
+        })
+    }
 }
 
 new App();
